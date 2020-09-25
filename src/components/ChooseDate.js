203916bc6b8b934685c0bbe701ff43e3d8d4eb64
Jasmine Silfver <jasmine.silfver@gmail.com>
2020-09-25 11:31:25 +0200

    FED19M1-3 Getting random data in simple if statement for now

--- src/components/ChooseDate.js
+++ src/components/ChooseDate.js
@@ -13,12 +13,26 @@ const ChooseDate = () => {
         // Each time the date changes, send to the db what date, and get back what times are available at that date, to then show the times in a dropdown
         //Add the times recieved from the db to the state array availableTimes for a check if the time should be disabled or not
         // Make sure you can't book on dates before today/earlier than the current time
-        setAvailableTimes([18, 21])
+
+        let random = Math.floor(Math.random() * 30)
+
+        if(random >= 30) {
+            setAvailableTimes([])
+        } else {
+            let random = Math.floor(Math.random() * 3)
+            if(random === 1) {
+                setAvailableTimes([18, 21])
+            } else if(random === 2) {
+                setAvailableTimes([18])
+            } else if(random === 3) {
+                setAvailableTimes([21])
+            }
+        }
     }, [value])
 
     // Get options of seats depending on amount of seats
     for(let i=1; i <= seats; i++) {
-        options.push(<option value={i}>{i}</option>)
+        options.push(<option key={i} value={i}>{i}</option>)
     }
 
     return ( 
@@ -31,7 +45,7 @@ const ChooseDate = () => {
                 />
 
                 <select name="times" id="times">
-                    <option disabled selected value> - Time - </option>
+                    <option disabled selected> - Time - </option>
                     <option value='18' disabled={availableTimes.includes(18) ? null : 'disabled'}>18:00</option>
                     <option value='21' disabled={availableTimes.includes(21) ? null : 'disabled'}>21:00</option>
                 </select>
@@ -40,6 +54,8 @@ const ChooseDate = () => {
                     <option disabled selected value> - Seats - </option>      
                     {options}
                 </select>
+
+                <button>Book</button>
             </form>
         </>
      );
