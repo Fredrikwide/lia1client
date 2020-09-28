648e64ccf94ca50686aa6de2a3b7b5de843ef093
Jasmine Silfver <jasmine.silfver@gmail.com>
2020-09-28 10:24:31 +0200

    FED19M1-3 Fixed issue with select, added change handler and a form values state, and a config file

--- src/components/ChooseDate.js
+++ src/components/ChooseDate.js
@@ -1,18 +1,33 @@
 import React, {useState, useEffect} from 'react'
 import Datepicker from 'react-date-picker'
+import config from '../config'
+
+/**
+ * TODO
+ * 
+ * Make sure you can't book on dates or time before current time
+ * 
+ * Add max months to pre-book?
+ */
 
 const ChooseDate = () => {
-
-    const [value, setValue] = useState(new Date());
+    const [date, setDate] = useState(new Date());
     const [availableTimes, setAvailableTimes] = useState([])
-    // Add how many seats per table to a config file
-    const seats = 6
+    const [formValues, setFormValues] = useState({
+        date: null,
+        time: '',
+        seats: 0
+    })
+
     let options = []
 
     useEffect(() => {
-        // Each time the date changes, send to the db what date, and get back what times are available at that date, to then show the times in a dropdown
-        //Add the times recieved from the db to the state array availableTimes for a check if the time should be disabled or not
-        // Make sure you can't book on dates before today/earlier than the current time
+        // Each time the date changes
+        // Send date to db
+        // Get available times
+        // Add times to the availableTimes state so that we can check if we need to disable the time option or not
+
+        setFormValues(f => ({...f, date, time: ''}))
 
         let random = Math.floor(Math.random() * 30)
 
@@ -28,10 +43,15 @@ const ChooseDate = () => {
                 setAvailableTimes([21])
             }
         }
-    }, [value])
+    }, [date])
+
+    // Handler to hopefully handle all the form changes and add them to the form state....... Except the date, I have to update that in useEffect.
+    const handleChange = (e) => {
+        setFormValues({...formValues, [e.target.name]: e.target.value})
+    }
 
     // Get options of seats depending on amount of seats
-    for(let i=1; i <= seats; i++) {
+    for(let i=1; i <= config.seats; i++) {
         options.push(<option key={i} value={i}>{i}</option>)
     }
 
@@ -39,19 +59,20 @@ const ChooseDate = () => {
         <>
             <p>Choose date and time</p>
             <form>
+                {/* Can't figure out how to change the date without calling setDate directly on onChange... */}
                 <Datepicker
-                    onChange={setValue}
-                    value={value}
+                    onChange={setDate}
+                    value={date}
                 />
 
-                <select name="times" id="times">
-                    <option disabled selected> - Time - </option>
+                <select name="time" id="time" onChange={handleChange} value={formValues.time}>
+                    <option value={''} disabled> - Time - </option>
                     <option value='18' disabled={availableTimes.includes(18) ? null : 'disabled'}>18:00</option>
                     <option value='21' disabled={availableTimes.includes(21) ? null : 'disabled'}>21:00</option>
                 </select>
 
-                <select name="seats" id="seats">
-                    <option disabled selected value> - Seats - </option>      
+                <select name="seats" id="seats" onChange={handleChange} value={formValues.seats}>
+                    <option value={0} disabled> - Seats - </option>      
                     {options}
                 </select>
 
---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+export default {
+	seats: 6
+}
