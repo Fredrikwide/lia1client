0b2417ef13d4366a73ae14984b68733a99d9e4cc
Fredrikwide <Fredrik.widegren@gmail.com>
2020-09-28 11:26:00 +0200

    added background image and some text

---
+++ src/components/navigation/Jumbo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './jumbo.scss'
+
+const Jumbo = () => {
+    return (
+        <div className="outer-wrapper">
+            <div className="inner-wrapper">
+                <div className="headOneText">
+                    <h1>I am the Hero</h1>
+                </div>
+                <div className="headTwoText">
+                    <h2>The coolest place in town</h2>
+                </div>
+                <div className="btn-wrapper">
+                    <button className="btnClick">Click me to Join</button>
+                </div>
+            </div>
+        </div>   
+    )
+}
+
+export default Jumbo
