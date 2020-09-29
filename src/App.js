0f23ae58f1e4b6c3140fda498279146367ac0f68
Fredrik Widegren <Fredrik.Widegren@gmail.com>
2020-09-29 18:53:00 +0200

    added new style to front-end page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/pages/Footer/Footer';
+import Home from './components/pages/HomePage/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      {/* <Footer /> */}
+    </Router>
+
   );
 }
 
---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Something Something',
+    headline: 'Awesomest DINNER ever',
+    description: 'That was Wintermute, manipulating the lock the way it had manipulated the drone micro and the drifting shoals of waste. The last Case saw of Chiba were the cutting edge, whole bodies of technique supplanted monthly, and still he’d see the matrix in his jacket pocket.',
+    buttonLabel: 'Book',
+    imgStart: '',
+
+}
+
