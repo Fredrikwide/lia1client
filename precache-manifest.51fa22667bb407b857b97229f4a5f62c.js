64dad06f960d9f6c929390f13ccdafce1a3775c7
fredrikwide <fredrik.widegren@gmail.com>
2020-10-22 16:16:56 +0200

    Updates

---
+++ precache-manifest.51fa22667bb407b857b97229f4a5f62c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec8035483952cb3358cb9f6fc8ce7a8",
+    "url": "/lia1client/index.html"
+  },
+  {
+    "revision": "0fed8db7711b1a04ca15",
+    "url": "/lia1client/static/css/2.3bf0a6c9.chunk.css"
+  },
+  {
+    "revision": "9b81cff13d5f6c255246",
+    "url": "/lia1client/static/css/main.610b8579.chunk.css"
+  },
+  {
+    "revision": "0fed8db7711b1a04ca15",
+    "url": "/lia1client/static/js/2.b0107dde.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/lia1client/static/js/2.b0107dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b81cff13d5f6c255246",
+    "url": "/lia1client/static/js/main.4c7932d6.chunk.js"
+  },
+  {
+    "revision": "dace81423c0e8f38787b",
+    "url": "/lia1client/static/js/runtime-main.795935f5.js"
+  },
+  {
+    "revision": "d11d43560f5ba2c0a14e7add654aa5bf",
+    "url": "/lia1client/static/media/restaurantHero.d11d4356.jpg"
+  }
+]);
