d5c0f07ebfc4b0b30c618e548e2d4785c5d9b930
fredrikwide <fredrik.widegren@gmail.com>
2020-10-23 11:27:28 +0200

    corrected auth route for token validation

--- src/services/authToken.js
+++ src/services/authToken.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
-
+const base_url = 'https://booking-app-lia1.herokuapp.com/'
+const admin_url = `${base_url}admin/`
 
 export const checkToken = async (token) => {
 
-  const tokenRes = await axios.post('http://localhost:5000/admin/validToken', null,
+  const tokenRes = await axios.post(`${admin_url}/validToken`, null,
     { headers: { "x-auth-token": token } }
   )
 
